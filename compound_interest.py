e75b816956a3d46f67d00a1c0dc2f8ec3e6c6b1f
khoeunkosalvireak <virakvary@gmail.com>
2022-04-19 21:24:44 +0700

    Done!

--- compound_interest.py
+++ compound_interest.py
@@ -1,6 +1,6 @@
 Loop = True
 while Loop:
-    print("All the input should be bigger than 1 and the number of month must be smaller than 36.Interest rate must be bigger than 2.5")
+    # All the input should be bigger than 1; Number of month must be smaller than 36; Interest rate must be smaller than 12
     A = float(input("Deposit: "))
     while A <= 0:
        A = float(input("Deposit: "))
@@ -8,7 +8,7 @@ while Loop:
     while N < 1 or N > 36:
        N = int(input("Duration of deposit in months: "))
     W = float(input("Interest_rate: "))
-    while W <= 2.5 or W > 12 :
+    while W <= 2.5 or W > 12:
         W = float(input("Interest_rate: "))
 
 
